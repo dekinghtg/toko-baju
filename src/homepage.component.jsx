9526fbad01154377d534cef7896749f4a53c023b
dekinghtg <deking.htg@gmail.com>
2020-12-04 08:58:05 +0700

    init toko baju

---
+++ src/homepage.component.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import './homepage.style.scss';
+
+const HomePage = () => (
+
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>TOPI</h1>
+                    <span className='subtitle'>BELI</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>JAKET</h1>
+                    <span className='subtitle'>BELI</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>SEPATU</h1>
+                    <span className='subtitle'>BELI</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>WANITA</h1>
+                    <span className='subtitle'>BELI</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>PRIA</h1>
+                    <span className='subtitle'>BELI</span>
+                </div>
+            </div>
+
+        </div>
+    </div>
+)
+
+export default HomePage;
