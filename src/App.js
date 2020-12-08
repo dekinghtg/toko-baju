2b97b4114f9310ad4254793691d448b8d4a5010a
dekinghtg <deking.htg@gmail.com>
2020-12-08 15:53:04 +0700

    update menu item

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import {Switch, Route} from 'react-router-dom'
 import HomePage from "./pages/homepage/homepage.component";
+
+const TopiPage = () => (
+    <div>
+        <h1>TOPI PAGE</h1>
+    </div>
+)
 
 function App() {
     return (
-        <HomePage/>
+        <div>
+            <Switch>
+                <Route exact path='/' component={HomePage}/>
+                <Route exact path='/topi' component={TopiPage}/>
+            </Switch>
+        </div>
     );
 }
 
