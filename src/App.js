8178875ffc8ed2f52fbe603dc2cb4c247d5960ea
dekinghtg <deking.htg@gmail.com>
2020-12-08 20:32:14 +0700

    add preview collection

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import './App.css';
 import {Switch, Route} from 'react-router-dom'
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
-const TopiPage = () => (
-    <div>
-        <h1>TOPI PAGE</h1>
-    </div>
-)
 
 function App() {
     return (
         <div>
             <Switch>
                 <Route exact path='/' component={HomePage}/>
-                <Route exact path='/topi' component={TopiPage}/>
+                <Route exact path='/toko' component={ShopPage}/>
             </Switch>
         </div>
     );
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -11,7 +11,7 @@ const MenuItem = ({title, imageUrl, size, history, linkUrl, match}) => (
         <div className='background-image' style={{backgroundImage: `url(${imageUrl})`}}/>
         <div className='content'>
             <h1 className='title'>{title.toUpperCase()}</h1>
-            <span className='subtitle'>BELI</span>
+            <span className='subtitle'>KE TOKO</span>
         </div>
     </div>
 );
